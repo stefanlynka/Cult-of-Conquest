44a7d8f69c9411050def06555d87cc6f21875cc2
Stefan Lynka <slynka@sfu.ca>
2019-10-04 18:01:43 -0700

    AI Frontier
    AI armies now moves to frontier when it has nothing else to do
    AI now properly retreats when it scouts humans

--- Assets/Scripts/Button Scripts/BattleButton.cs
+++ Assets/Scripts/Button Scripts/BattleButton.cs
@@ -18,27 +18,34 @@ public class BattleButton : MonoBehaviour{
     }
 
     private void OnMouseDown() {
+        print("Is it AI attacking Human: " + battleMenu.GetComponent<BattleMenu>().AIAttackingHuman());
         if (battleMenu.GetComponent<BattleMenu>().AIAttackingHuman() && buttonType != "back") {
+            print("AI is attacking human");
             if (battleMenu.GetComponent<BattleMenu>().AIWantsToFight()) {
+                print("AI wants to fight");
                 if (buttonType == "instant") battleMenu.GetComponent<BattleMenu>().InstantBattle();
                 if (buttonType == "simulation") battleMenu.GetComponent<BattleMenu>().StartSimulation();
             }
-            else battleMenu.GetComponent<BattleMenu>().Retreat();
+            else {
+                print("AI retreating");
+                battleMenu.GetComponent<BattleMenu>().Retreat();
+            }
         }
-
-        if (buttonType == "simulation" && !battleMenu.GetComponent<BattleMenu>().inSimulation && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
-            //print("enter simulation");
-            battleMenu.GetComponent<BattleMenu>().StartSimulation();
-        }
-        else if (buttonType == "instant" && !battleMenu.GetComponent<BattleMenu>().inSimulation && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
-            //print("enter instant");
-            battleMenu.GetComponent<BattleMenu>().InstantBattle();
-        }
-        else if (buttonType == "retreat" && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
-            battleMenu.GetComponent<BattleMenu>().Retreat();
-        }
-        else if (buttonType == "back" && battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
-            battleMenu.GetComponent<BattleMenu>().ExitMenu();
+        else {
+            if (buttonType == "simulation" && !battleMenu.GetComponent<BattleMenu>().inSimulation && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
+                //print("enter simulation");
+                battleMenu.GetComponent<BattleMenu>().StartSimulation();
+            }
+            else if (buttonType == "instant" && !battleMenu.GetComponent<BattleMenu>().inSimulation && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
+                //print("enter instant");
+                battleMenu.GetComponent<BattleMenu>().InstantBattle();
+            }
+            else if (buttonType == "retreat" && !battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
+                battleMenu.GetComponent<BattleMenu>().Retreat();
+            }
+            else if (buttonType == "back" && battleMenu.GetComponent<BattleMenu>().IsBattleOver()) {
+                battleMenu.GetComponent<BattleMenu>().ExitMenu();
+            }
         }
 
     }
